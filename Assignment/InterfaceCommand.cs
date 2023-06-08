1bb86afdaacf871985df652acb3879595ee66027
arsh052003 <122691385+arsh052003@users.noreply.github.com>
2023-06-08 17:48:00 +0000

    Updated InterfaceCommand

--- Assignment/InterfaceCommand.cs
+++ Assignment/InterfaceCommand.cs
@@ -1,36 +1,36 @@
 ﻿namespace Assignment.InterfaceCommand;
 
-interface IRobotCommand
+public interface IRobotCommand
 {
     void Run(Robot robot);
 }
 
-class OffCommand : IRobotCommand
+public class OffCommand : IRobotCommand
 {
     public void Run(Robot robot) => robot.IsPowered = false;
 }
 
-class OnCommand : IRobotCommand
+public class OnCommand : IRobotCommand
 {
     public void Run(Robot robot) => robot.IsPowered = true;
 }
 
-class WestCommand : IRobotCommand
+public class WestCommand : IRobotCommand
 {
     public void Run(Robot robot) { if (robot.IsPowered) robot.X--; }
 }
 
-class EastCommand : IRobotCommand
+public class EastCommand : IRobotCommand
 {
     public void Run(Robot robot) { if (robot.IsPowered) robot.X++; }
 }
 
-class SouthCommand : IRobotCommand
+public class SouthCommand : IRobotCommand
 {
     public void Run(Robot robot) { if (robot.IsPowered) robot.Y--; }
 }
 
-class NorthCommand : IRobotCommand
+public class NorthCommand : IRobotCommand
 {
     public void Run(Robot robot) { if (robot.IsPowered) robot.Y++; }
 }
