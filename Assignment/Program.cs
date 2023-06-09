3c1ed6c827d078bc5ed863be3cc1e684911b0087
arsh052003 <122691385+arsh052003@users.noreply.github.com>
2023-06-09 18:54:11 +0000

    Documentation and Commenting of Program.cs

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,14 +4,17 @@
     {
         static void Main()
         {
-            // Run your RobotTester class here -> RobotTester.TestRobot()
+            //Run your RobotTester class here -> RobotTester.TestRobot()
             InterfaceCommand.OnCommand onCommand = new InterfaceCommand.OnCommand();
+            //creates a new instance of the Robot class with a capacity of 2 commands 
             Robot robot1 = new(2);
+            //Loads an instance of the OnCommand class into the robot1 object 
             robot1.LoadCommand(new InterfaceCommand.OnCommand());
+            //Creates a new instance of the RobotTester class, passing a new instance of the Robot class with a capacity of 5 commands
             RobotTester robotTester = new(new Robot(5));
+            //Calling the Giveinstructions method  and ExecuteCommand method of the robotTester object
             robotTester.Giveinstructions();
             robotTester.ExecuteCommands();
-
         }
     }
 }
